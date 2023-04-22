a007b57f538bb3a2efb534d9de4474a9428eedbf
Loizos <loizosnic99@gmail.com>
2023-04-22 23:40:01 +0300

    More tests

---
+++ Unit-test/serial.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the server URL and port
+SERVER="localhost"
+PORT="30000"
+
+# Define the URL to be used in the requests
+URL="https://${SERVER}:${PORT}/dir1/dir2/test.txt"
+
+# Use a loop to make 40 concurrent requests
+for i in {1..200}; do
+  curl --insecure -s -o /dev/null -w "%{http_code}\n" $URL &
+done
+
+# Wait for all requests to finish
+wait
+
